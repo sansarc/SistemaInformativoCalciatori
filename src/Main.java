e2f60333ad0276a220cdcf733748413db428b816
Arcangelo Sanseviero <sansevieroarcangelo@gmail.com>
2023-12-03 00:24:27 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,56 @@
-import Calciatore.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main extends JFrame {
     private JPanel panel;
-    private JLabel ruoloLabel;
-    private JComboBox<String> ruoliComboBox;
-    private JLabel titleLabel;
+    private JLabel positionLabel;
+    private JComboBox<String> positionComboBox;
     private JButton searchButton;
     private JTable resultsTable;
     private JScrollPane tableScrollPane;
+    private JTextField nameTextField;
+    private JLabel nameLabel;
+    private JTextField lastnameTextField;
+    private JLabel lastNameLabel;
+    private JLabel footLabel;
+    private JComboBox<String> footComboBox;
+    private JLabel ageLabel;
+    private JTextField ageTextField;
+    private JCheckBox retiredCheckBox;
 
     public Main() {
         setContentPane(panel);
         setTitle("Sistema Informativo Calciatori");
-        pack();
-        setDefaultCloseOperation(HIDE_ON_CLOSE);     // TODO: change it to EXIT_ON_CLOSE
+        setSize(900, 600);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);     // TODO: cambia a EXIT_ON_CLOSE
         setLocationRelativeTo(null);
         setVisible(true);
 
-        String[] ruoli = new String[] {"Seleziona un ruolo", "Forward", "Midfielder", "Defender", "Goalkeeper"};
-        for (String i : ruoli) ruoliComboBox.addItem(i);
+        String[] positions = new String[] {"", "Forward", "Midfielder", "Defender", "Goalkeeper"};
+        for (String i : positions) positionComboBox.addItem(i);
+        footComboBox.addItem("");
+        footComboBox.addItem("Dx");
+        footComboBox.addItem("Sx");
 
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String ruolo = (String) ruoliComboBox.getSelectedItem();
-                Query query = new Query(resultsTable);
-                boolean state = query.getCalciatoriByRuolo(ruolo);
-            }
-        });
+                String position = (String) positionComboBox.getSelectedItem();
+                String name = nameTextField.getText();
+                String lastname = lastnameTextField.getText();
+                char foot = footComboBox.getSelectedItem().equals("Dx") ? 'D' : 'S';
+                String age = ageTextField.getText();
+                boolean isRetired = retiredCheckBox.isSelected();
 
-        ruoliComboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String ruolo = (String) ruoliComboBox.getSelectedItem();
-                searchButton.setEnabled(!ruolo.equals("Seleziona un ruolo"));
+                if (position.isBlank() && name.isBlank() && lastname.isBlank() && foot == '\0' && !isRetired) JOptionPane.showMessageDialog(Main.this, "Ricerca non valida, riprova.");
+                else {
+                    Query query = new Query(resultsTable);
+                    query.getSearch(name, lastname, age, position, foot, isRetired);        // TODO: aggiungere campo Squadra
+                }
             }
         });
     }
-
 
     public static void main(String[] args) {
         new Main();
--- src/Query.java
+++ src/Query.java
@@ -1,40 +1,43 @@
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import Calciatore.*;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class Query {
-    JTable resultsTable;
+    private JTable resultsTable;
     public Query(JTable resultsTable) {
         this.resultsTable = resultsTable;
     }
-    public boolean getCalciatoriByRuolo(String ruolo) {
+
+    public void getSearch(String name, String lastname, String age, String position, char foot, boolean isRetired) {
+        String query = getQuery(name, lastname, age, position, foot, isRetired);
         Connection connection = DBconnection.connect();
-        String query = "SELECT nome, cognome FROM calciatore WHERE ruoli = ?";
 
         try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, ruolo);
+            int index = 1;
+
+            if (!name.isBlank()) statement.setString(index++, name);                // parsing dei valori inseriti nella query
+            if (!lastname.isBlank()) statement.setString(index++, lastname);
+            if (!position.isBlank()) statement.setString(index++, position);
+            if (!age.isBlank()) statement.setString(index++, age);
+            statement.setString(index, String.valueOf(foot));
+            System.out.println(statement);
             ResultSet resultSet = statement.executeQuery();
 
-            DefaultTableModel tableModel = new DefaultTableModel();
+            DefaultTableModel tableModel = new DefaultTableModel();     // tabella di output
+            ResultSetMetaData metaData = resultSet.getMetaData();       // dati del risultato della query
 
-            // Get metadata to set column names
-            ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
-
-            // Add column names to the table model
-            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                tableModel.addColumn(metaData.getColumnName(columnIndex));
+            for (int i = 1; i <= columnCount; i++) {              // per ogni colonna, eventualmente modifica il suo nome con la funzione mapToCustomColumnName ad aggiungila alla tabella di output
+                String columnName = metaData.getColumnName(i);
+                String customColumnName = mapToCustomColumnName(columnName);
+                tableModel.addColumn(customColumnName);
             }
 
-            // Add data rows to the table model
-            while (resultSet.next()) {
+            while (resultSet.next()) {                      // inserisci i dati nella tabella
                 Object[] rowData = new Object[columnCount];
-                for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                    rowData[columnIndex - 1] = resultSet.getObject(columnIndex);
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    rowData[i - 1] = resultSet.getObject(columnName);
                 }
                 tableModel.addRow(rowData);
             }
@@ -44,10 +47,69 @@ public class Query {
 
         catch (SQLException se) {
             se.printStackTrace();
-        } finally {
+        }
+        finally {
             DBconnection.disconnect(connection);
         }
+    }
 
-        return true;
+    private String getQuery(String name, String lastname, String age, String position, char foot, boolean isRetired) {          // costruzione della query in base ai valori inseriti
+        StringBuilder query = new StringBuilder("SELECT * FROM calciatore WHERE");
+        boolean conditionAdded = false;         // condizione per verificare se la query deve aggiornare altri valori e aggiungere AND, oppure se la query è terminata
+
+        if (!name.isBlank()) {
+            query.append(" nome = ?");
+            conditionAdded = true;
+        }
+        if (!lastname.isBlank()) {
+            if (conditionAdded) query.append(" AND");
+            query.append(" cognome = ?");
+            conditionAdded = true;
+        }
+        if (!position.isBlank()) {
+            if (conditionAdded) query.append(" AND");
+            query.append(" ruoli = ?");
+            conditionAdded = true;
+        }
+        if (!age.isBlank()) {
+            if (conditionAdded) query.append(" AND");
+            query.append(" YEAR(CURDATE()) - YEAR(datanascita) = ?");           // verifica se anno attuale - anno della data di nascita = l'età inserita nel form
+            conditionAdded = true;
+        }
+        if (foot != '\0') {
+            if (conditionAdded) query.append(" AND");
+            query.append(" piede = ?");
+            conditionAdded = true;
+        }
+        if (isRetired) {
+            if (conditionAdded) query.append(" AND");
+            query.append(" dataritiro IS NOT NULL");
+        }
+
+        query.append(";");
+        return query.toString();
+    }
+
+    private String mapToCustomColumnName(String columnName) {           // modifica dei nomi delle colonne della tabella di output (altrimenti nome della colonna del database)
+        switch (columnName) {
+            case "nome":
+                return "Nome";
+            case "cognome":
+                return "Cognome";
+            case "ruoli":
+                return "Ruoli";
+            case "datanascita":
+                return "Nato";
+            case "piede":
+                return "Piede preferito";
+            case "dataritiro":
+                return "Ritirato";
+            case "golfatti":
+                return "Gol Segnati";
+            case "golsubiti":
+                return "Gol Subiti";
+            default:
+                return columnName;
+        }
     }
 }
